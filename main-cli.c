95b6cc12ae9dbcda6d854682c1ab0539efef0972
Andrew Poelstra (local) <apoelstra@wpsoftware.net>
2012-01-05 16:16:58 -0800

    Add cli version of ramsey, add -O3 flag

---
+++ main-cli.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "global.h"
+#include "ramsey.h"
+#include "stream.h"
+
+struct _global_data global;
+
+int main (int argc, char *argv[])
+{
+  struct _global_data *defs = set_defaults ();
+  Stream *stm = file_stream_new ("r");
+
+  if (argc > 1)
+    {
+      if (stm->open (stm, argv[1]) == NULL)
+        {
+          fprintf (stderr, "Failed to open script ``%s''\n", argv[1]);
+          exit (EXIT_FAILURE);
+        }
+    }
+  else stm->_data = stdin;
+
+  defs->in_stream = stm;
+  defs->out_stream = file_stream_new ("w");
+  defs->out_stream->_data = stdout;
+  process (defs);
+
+  return 0;
+}
+
