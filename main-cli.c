569de740d402bb296fe042140bbafdff29115e9a
Andrew Poelstra (local) <apoelstra@wpsoftware.net>
2012-01-18 21:51:22 -0800

    Make CLI version able to take multiple file inputs
    Also output "Interactive mode" when reading stdin

--- main-cli.c
+++ main-cli.c
@@ -12,26 +12,37 @@ struct _global_data global;
 
 int main (int argc, char *argv[])
 {
-  struct _global_data *defs = set_defaults ();
+  srand (time (NULL));
 
-  if (argc > 1)
+  if (argc < 2)
+    { 
+      struct _global_data *defs = set_defaults ();
+      defs->out_stream = stdout_stream_new ();
+      defs->in_stream = stdin_stream_new ();
+
+      puts ("Welcome to RamseyScript CLI " VERSION);
+      puts ("All code used in this project is public domain.");
+      puts ("");
+      puts ("Interactive mode.");
+
+      process (defs);
+    }
+  else
     {
-      defs->in_stream = file_stream_new (argv[1]);
-      if (!defs->in_stream->open (defs->in_stream, STREAM_READ))
+      int i;
+      for (i = 1; argv[i]; ++i)
         {
-          fprintf (stderr, "Failed to open script ``%s''\n", argv[1]);
-          exit (EXIT_FAILURE);
+          struct _global_data *defs = set_defaults ();
+          defs->out_stream = stdout_stream_new ();
+          defs->in_stream = file_stream_new (argv[i]);
+          if (!defs->in_stream->open (defs->in_stream, STREAM_READ))
+            fprintf (stderr, "Failed to open script ``%s''\n", argv[i]);
+          else
+            process (defs);
+          defs->in_stream->destroy (defs->in_stream);
+          defs->out_stream->destroy (defs->out_stream);
         }
     }
-  else defs->in_stream = stdin_stream_new ();
-  defs->out_stream = stdout_stream_new ();
-
-  puts ("Welcome to RamseyScript CLI " VERSION);
-  puts ("All code used in this project is public domain.");
-  puts ("");
-
-  srand (time (NULL));
-  process (defs);
 
   return 0;
 }
