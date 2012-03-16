19ed7a768dd76c1ffccc87b745c4c293bdf1e612
Andrew Poelstra (local) <apoelstra@wpsoftware.net>
2012-03-16 12:40:33 -0700

    Doxygenate recurse.h and process.h

--- process.h
+++ process.h
@@ -12,10 +12,27 @@
  * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 
+/*! \file process.h
+ *  \brief Defines script-processing and parsing functions.
+ */
+
 #ifndef PROCESS_H
 #define PROCESS_H
 
+/*! \brief Sets default variables before a script run.
+ *
+ *  \param [in] in   The input stream to use.
+ *  \param [in] out  The output stream to use.
+ *  \param [in] err  The error stream to use.
+ *
+ *  \return A newly-allocated global_data struct, or NULL on failure.
+ */
 struct _global_data *set_defaults (stream_t *in, stream_t *out, stream_t *err);
+
+/*! \brief Reads and runs a script from the global state's input stream.
+ *
+ *  \param [in] state  The global state of the program.
+ */
 void process (struct _global_data *state);
 
 #endif
--- recurse.h
+++ recurse.h
@@ -12,14 +12,41 @@
  * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 
+/*! \file recurse.h
+ *  \brief Defines generic recursion-handling functions.
+ */
+
 #ifndef RECURSE_H
 #define RECURSE_H
 
 #include "global.h"
 
+/*! \brief Recursion checks to run before ramsey_t->recurse().
+ *
+ *  \param [in] rt    The Ramsey object that is being recursed on.
+ *  \param [in] state The global state of the program.
+ *
+ *  \return 1 if recursion should be done, 0 if it should be stopped.
+ */
 int recursion_preamble (ramsey_t *rt, global_data_t *state);
+
+/*! \brief Recursion checks to run after ramsey_t->recurse().
+ *
+ *  \param [in] rt    The Ramsey object that is being recursed on.
+ */
 void recursion_postamble (ramsey_t *rt);
+
+/*! \brief Zero out recursion-related variables of a ramsey_t.
+ *
+ *  \param [in] rt    The Ramsey object that is being recursed on.
+ */
 void recursion_init (ramsey_t *rt);
+
+/*! \brief Set recursion-related variables of a ramsey_t from script variables.
+ *
+ *  \param [in] rt    The Ramsey object that is being recursed on.
+ *  \param [in] state The global state of the program.
+ */
 void recursion_reset (ramsey_t *rt, global_data_t *state);
 
 #endif
