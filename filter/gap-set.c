5b495d24a8ab31cc918a79fe31d3aa3d8e7f67cf
Andrew Poelstra (local) <apoelstra@wpsoftware.net>
2012-03-17 19:01:14 -0700

    Cleanup gap-set filter, add support for coloring-based gap sets

--- filter/gap-set.c
+++ filter/gap-set.c
@@ -27,23 +27,46 @@ struct _priv {
   bool has_symmetry;
 };
 
-static bool check_gap_set (const filter_t *f, const ramsey_t *rt)
-{
-  (void) f;
-  (void) rt;
-  return 1;
-}
-
 static bool cheap_check_gap_set (const filter_t *f, const ramsey_t *rt)
 {
   struct _priv *priv = (struct _priv *) f;
-  const int *data = rt->get_priv_data_const (rt);
+  const ramsey_t *seq, *gaps;
+  const int *data;
+  int len;
 
   assert (f != NULL);
   assert (rt != NULL);
 
-  return (rt->get_length(rt) < 2 || priv->gap_set->find_value
-             (priv->gap_set, data[rt->get_length(rt) - 1] - data[rt->get_length(rt) - 2]));
+  /* Select the last-modified color, if applicable */
+  if (rt->type == TYPE_COLORING)
+    {
+      const int *cols = rt->get_alt_priv_data_const (rt);
+      int last_col = cols[rt->get_length (rt) - 1];
+      seq = ((const ramsey_t **) rt->get_priv_data_const (rt)) [last_col];
+
+      if (priv->gap_set->type == TYPE_COLORING)
+        {
+          if (priv->gap_set->get_n_cells (priv->gap_set) <= last_col)
+            return 1;
+          else
+            gaps = ((const ramsey_t **) priv->gap_set->get_priv_data_const (priv->gap_set))[last_col];
+        }
+      else
+        gaps = priv->gap_set;
+    }
+  else
+    {
+      seq = rt;
+      if (priv->gap_set->type == TYPE_COLORING)
+        gaps = ((const ramsey_t **) priv->gap_set->get_priv_data_const (priv->gap_set))[0];
+      else
+        gaps = priv->gap_set;
+    }
+
+  data = seq->get_priv_data_const (seq);
+  len  = seq->get_length (seq);
+
+  return (len < 2 || gaps->find_value (gaps, data[len - 1] - data[len - 2]));
 }
 
 /* end ACTUAL FILTER CODE */
@@ -63,18 +86,18 @@ static bool _filter_supports (const filter_t *flt, e_ramsey_type type)
   (void) flt;
   return type == TYPE_WORD ||
          type == TYPE_PERMUTATION ||
-         type == TYPE_SEQUENCE;
+         type == TYPE_SEQUENCE ||
+         type == TYPE_COLORING;
 }
 
 static bool _filter_set_mode (filter_t *flt, e_filter_mode mode)
 {
-  flt->mode = mode;
-  switch (mode)
-    {
-    case MODE_FULL:      flt->run  = check_gap_set; break;
-    case MODE_LAST_ONLY: flt->run  = cheap_check_gap_set; break;
-    }
-  return 1;
+  flt->mode = MODE_LAST_ONLY;
+  flt->run  = cheap_check_gap_set;
+  if (mode != MODE_LAST_ONLY)
+    fprintf (stderr, "Warning: enabling full-check on unsupported filter ``%s''\n",
+             flt->get_type (flt));
+  return (mode == MODE_LAST_ONLY);
 }
 
 /* CONSTRUCTOR / DESTRUCTOR  */
