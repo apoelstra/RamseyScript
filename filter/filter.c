b392fa09b703372d8a028668a6b812b715bf16c7
Andrew Poelstra <apoelstra@wpsoftware.net>
2013-05-22 16:52:42 -0700

    Add pythagorean triple filter

--- filter/filter.c
+++ filter/filter.c
@@ -31,6 +31,7 @@
 #include "no-odd-lattice-aps.h"
 #include "no-rainbow-aps.h"
 #include "no-schur-solutions.h"
+#include "no-pythagorean-triples.h"
 static const parser_t g_filter[] = {
   { "gap_set",             "Eliminate sequences whose gaps are outside of gap_set.", filter_gap_set_new },
   { "no_double_3_aps",     "Eliminate objects containing double 3-AP's.",  filter_double_3_ap_new },
@@ -41,6 +42,7 @@ static const parser_t g_filter[] = {
   { "no_n_aps",            "Eliminate objects containing n-AP's, with n == ap_length", filter_n_ap_new },
   { "no_odd_lattice_aps",  "Eliminate lattices containing monochromatic lines.", filter_odd_lattice_ap_new },
   { "no_rainbow_aps",      "Eliminate colorings containing rainbow AP's.", filter_rainbow_ap_new },
+  { "no_pythagorean_triples", "Eliminate objects with solutions to X^2 + Y^2 = Z^2.", filter_pythag_new },
   { "no_schur_solutions",  "Eliminate objects with solutions to X + Y = Z.", filter_schur_new },
   { "no_consecutive_numbers", "Eliminate objects with consecutive numbers.", filter_consecutive_new }
 };
---
+++ filter/no-pythagorean-triples.c
@@ -0,0 +1,98 @@
+/* RamseyScript
+ * Written in 2012 by
+ *   Andrew Poelstra <apoelstra@wpsoftware.net>
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to
+ * the public domain worldwide. This software is distributed without
+ * any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "filter.h"
+
+static bool check_pythag (const filter_t *f, const ramsey_t *rt)
+{
+  int len = rt->get_length (rt);
+  const int *val = rt->get_priv_data_const (rt);
+  int i, j, k;
+
+  assert (val != NULL);
+  (void) f;
+
+  for (i = 0; i < len; ++i)
+    for (j = i; j < len; ++j)
+      for (k = j; k < len; ++k)
+        if (val[i]*val[i] == val[j]*val[j] + val[k]*val[k] ||
+            val[j]*val[j] == val[i]*val[i] + val[k]*val[k] ||
+            val[k]*val[k] == val[i]*val[i] + val[k]*val[k])
+          return 0;
+  return 1;
+}
+
+static bool cheap_check_pythag (const filter_t *f, const ramsey_t *rt)
+{
+  int len = rt->get_length (rt);
+  const int *val = rt->get_priv_data_const (rt);
+  int i, j;
+
+  assert (val != NULL);
+  (void) f;
+
+  for (i = 0; i < len; ++i)
+    for (j = i; j < len; ++j)
+      if (val[j]*val[j] == val[i]*val[i] + val[len - 1]*val[len - 1] ||
+          val[i]*val[i] == val[j]*val[j] + val[len - 1]*val[len - 1] ||
+          val[len - 1]*val[len - 1] == val[i]*val[i] + val[j]*val[j])
+        return 0;
+  return 1;
+}
+
+/* end ACTUAL FILTER CODE */
+static const char *_filter_get_type (const filter_t *flt)
+{
+  (void) flt;
+  return "no-pythagorean-triples";
+}
+
+static bool _filter_supports (const filter_t *flt, e_ramsey_type type)
+{
+  (void) flt;
+  return type == TYPE_WORD ||
+         type == TYPE_PERMUTATION ||
+         type == TYPE_SEQUENCE;
+}
+
+static bool _filter_set_mode (filter_t *flt, e_filter_mode mode)
+{
+  flt->mode = mode;
+  switch (mode)
+    {
+    case MODE_FULL:      flt->run  = check_pythag; break;
+    case MODE_LAST_ONLY: flt->run  = cheap_check_pythag; break;
+    }
+  return 1;
+}
+
+/* CONSTRUCTOR / DESTRUCTOR  */
+void *filter_pythag_new (const setting_list_t *vars)
+{
+  filter_t *rv = filter_new_generic ();
+  (void) vars;
+  rv->mode = MODE_LAST_ONLY;
+  rv->get_type = _filter_get_type;
+  rv->supports = _filter_supports;
+  rv->set_mode = _filter_set_mode;
+  rv->run  = cheap_check_pythag;
+  return rv;
+}
+
